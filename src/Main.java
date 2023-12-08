1cad4e4afa1c440fe79e9cf6d96cbc8b4688a650
matheus mozart <mmsnborges@gmail.com>
2023-12-08 19:29:04 -0400

    first commit

---
+++ src/Main.java
@@ -0,0 +1,31 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		File file = new File("/home/matheus/Documentos/in");
+		Scanner sc = null;
+		
+		try {
+			sc = new Scanner(file);
+			while(sc.hasNextLine()) {
+				System.out.println(sc.nextLine());
+			}
+		}
+		catch (IOException e){
+			System.out.println("Error: " + e.getMessage());
+		}
+		finally{
+			if(sc != null) {
+				sc.close();
+			}
+			
+		}
+		
+
+	}
+
+}
